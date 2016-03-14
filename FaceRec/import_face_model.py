2e461e1d45ad8a404d36b7b1edc91b8b06cc957b
Evamy <antriksh5235@gmail.com>
2016-03-14 11:44:54 +0530

    starting project finally

--- FaceRec/import_face_model.py
+++ FaceRec/import_face_model.py
@@ -1,15 +1,16 @@
 import sys
-import caffe
+# import caffe
 import cv2
-from PIL import Image
+# from PIL import Image
 import numpy as np
 from scipy.misc import imresize
-caffe.set_mode_cpu()
+# caffe.set_mode_cpu()
 
-home = "/home/walle/"
+# home = "/home/walle/"
 
-MODEL_FILE = home + 'models/vgg_face_caffe/VGG_FACE_deploy.prototxt'
-PRETRAINED = home + 'models/vgg_face_caffe/VGG_FACE.caffemodel'
+# MODEL_FILE = home + 'models/vgg_face_caffe/VGG_FACE_deploy.prototxt'
+# PRETRAINED = home + 'models/vgg_face_caffe/VGG_FACE.caffemodel'
 
-net = caffe.Net(MODEL_FILE, caffe.TEST)
-net.params['conv1_1']
+# net = caffe.Net(MODEL_FILE, caffe.TEST)
+# net.params['conv1_1']
+
