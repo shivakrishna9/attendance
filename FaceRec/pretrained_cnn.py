4adda6d1c0a3e02f8a79511a5cb10d1979d8da72
sara-02 <sarahmasud02@gmail.com>
2016-03-15 10:45:47 +0530

    trying to convert to h5

--- FaceRec/pretrained_cnn.py
+++ FaceRec/pretrained_cnn.py
@@ -1,11 +1,21 @@
 import numpy as np
-import cv2
-import theano
+# import cv2
+# import theano
 import scipy.io
+import json
 
-home = "/home/walle/"
-PRETRAINED = home + "models/vgg-face.mat"
+import numpy as np
+import h5py 
+import tables
+
+# home = "/home/walle/"
+PRETRAINED = "vgg_face_matconvnet/vgg_face.mat"
 
 def pretrained_cnn():
-	cnn = scipy.io.loadmat(PRETRAINED)['layers']
-	print cnn
+	cnn = scipy.io.loadmat(PRETRAINED)
+
+	with h5py.File('vgg_face.h5','w') as f:
+		net = cnn['net'].tolist()
+		f.create_dataset('vgg_face',data=net)
+
+pretrained_cnn()
