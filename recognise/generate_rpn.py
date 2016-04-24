49abb8a2312f6ac906eb21e77e7f753a9ca956a7
Evamy <antriksh5235@gmail.com>
2016-04-24 21:53:48 +0530

    rpn library added, trying to run resnet+faster-rcnn

--- recognise/generate_rpn.py
+++ recognise/generate_rpn.py
@@ -2,6 +2,14 @@ from rpn.generate import imdb_proposals
 from datasets.factory import get_imdb
 
 def generate_rpn():
+
+	home = "/home/walle/"
+
+    MODEL_FILE = home + 'Attendance/extras/models/ResNet-101-deploy.prototxt'
+    PRETRAINED = home + 'Attendance/extras/models/ResNet-101-model.caffemodel'
+
+    net = caffe.Net(MODEL_FILE, PRETRAINED, caffe.TRAIN)
+
 	imdb = get_imdb(args.imdb_name)
     imdb_boxes = imdb_proposals(net, imdb)
 
@@ -10,3 +18,8 @@ def generate_rpn():
     with open(rpn_file, 'wb') as f:
         cPickle.dump(imdb_boxes, f, cPickle.HIGHEST_PROTOCOL)
     print 'Wrote RPN proposals to {}'.format(rpn_file)
+
+
+if __name__ == '__main__':
+	generate_rpn()
+	
