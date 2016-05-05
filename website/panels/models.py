46b8fa5e4cb576d6f276f9f4306829526467ed0b
Zishan Ahmad <zishanahmad@outlook.com>
2016-05-05 16:53:15 +0530

    added models

--- website/panels/models.py
+++ website/panels/models.py
@@ -3,3 +3,32 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    course = models.CharField(max_length=20)
+    year = models.IntegerField(max_length=4)
+    semester = models.SmallIntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+    teacher = models.ForeignKey(Teacher)
+
+    def __str__(self):
+        return self.name
+
+class Studies(models.Model):
+    student_name = models.ForeignKey(Student)
+    subject_name = models.ForeignKey(Subject)
+    attendance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.subject_name + " <-> " + self.student_name
