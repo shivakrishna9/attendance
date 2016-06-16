1099452df60cbef966a3e8a6896ff9dec504ad30
Evamy <antriksh5235@gmail.com>
2016-06-16 13:25:34 +0530

    installers

---
+++ install1.sh
@@ -0,0 +1,6 @@
+pip install pip -U
+pip install numpy -U
+pip install pandas -U
+pip install theano
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0rc0-cp27-none-linux_x86_64.whl
+pip install keras
