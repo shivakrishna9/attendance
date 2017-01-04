795bc46b208f11d910d47f7d0e7b10756fc35f7c
Antriksh Agarwal <evamy@cometnet-10-21-46-163.utdallas.edu>
2017-01-04 10:07:47 -0600

    facescrub added to this repo

--- install1.sh
+++ install1.sh
@@ -1,6 +1,9 @@
 pip install pip -U
 pip install numpy -U
 pip install pandas -U
+pip install h5py -U
 pip install theano
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0rc0-cp27-none-linux_x86_64.whl
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.10.0-py2-none-any.whl
+pip install $TF_BINARY_URL
 pip install keras
